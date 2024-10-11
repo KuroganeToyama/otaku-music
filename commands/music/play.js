523c40f43642db0627a1c087cece474aba570d6f
KuroganeToyama <dingyen96@gmail.com>
2024-10-11 22:31:54 +0000

    Added ability to add single track and add playlist to queue

--- commands/music/play.js
+++ commands/music/play.js
@@ -4,51 +4,110 @@ const { QueryType } = require("discord-player");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
-        .setDescription('Play a song of your choice!')
-        .addStringOption(option =>
-            option.setName('song')
-                .setDescription('The title of the song you want to play')
-                .setRequired(true)
+        .setDescription('Play music of your choice!')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('track')
+                .setDescription('Add a single track to the queue')
+                .addStringOption(option =>
+                    option
+                        .setName('query')
+                        .setDescription('Query to search for track')
+                        .setRequired(true))
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('list')
+                .setDescription('Add a playlist to the queue')
+                .addStringOption(option =>
+                    option
+                        .setName('query')
+                        .setDescription('Query to search for playlist')
+                        .setRequired(true))
         ),
     
     async execute(interaction, player) {
         await interaction.deferReply();
 
-        // Search for song from Soundcloud
-        const song = interaction.options.getString('song');
-        const searchResult = await player
-            .search(song, {
-                requestedBy: interaction.user,
-                searchEngine: QueryType.AUTO
-            })
-            .catch(() => {});
+        if (interaction.options.getSubcommand() === 'track') {
+            // Search for track from SoundCloud
+            const query = interaction.options.getString('query');
+            const searchResult = await player
+                .search(query, {
+                    requestedBy: interaction.user,
+                    searchEngine: QueryType.AUTO
+                })
+                .catch(() => {});
 
-        // If search turns up nothing
-        if (!searchResult || !searchResult.tracks.length) {
-            await interaction.followUp({ content: "No results were found!" });
-            return;
-        }
-
-        const queue = player.nodes.create(interaction.guild, {
-            metadata: interaction.channel
-        });
-
-        try {
-            if (!queue.connection) {
-                await queue.connect(interaction.member.voice.channel);
+            // If search turns up nothing
+            if (!searchResult || !searchResult.tracks.length) {
+                await interaction.followUp({ content: "No results were found!" });
+                return;
             }
-        } 
-        catch {
-            player.nodes.delete(interaction.guildId);
-            await interaction.followUp({ content: "Could not join your voice channel!" });
+
+            const queue = player.nodes.create(interaction.guild, {
+                metadata: interaction.channel
+            });
+
+            try {
+                if (!queue.connection) {
+                    await queue.connect(interaction.member.voice.channel);
+                }
+            } 
+            catch {
+                player.nodes.delete(interaction.guildId);
+                await interaction.followUp({ content: "Could not join your voice channel!" });
+                return;
+            }
+
+            const track = searchResult.tracks[0];
+            await interaction.followUp({ content: `⏱ | Loaded track **${track.title}** for query **${query}**` });
+            queue.addTrack(track);
+            if (!queue.isPlaying()) {
+                await queue.node.play();
+            }
             return;
         }
 
-        const track = searchResult.tracks[0];
-        await interaction.followUp({ content: `⏱ | Loaded **${track.title}**` });
-        queue.addTrack(track);
-        if (!queue.isPlaying()) {
-            await queue.node.play();
+        if (interaction.options.getSubcommand() === 'list') {
+            // Search for playlist from SoundCloud
+            const query = interaction.options.getString('query');
+            const searchResult = await player
+                .search(query, {
+                    requestedBy: interaction.user,
+                    searchEngine: QueryType.AUTO
+                })
+                .catch(() => {});
+
+            // If search turns up nothing
+            if (!searchResult || !searchResult.tracks.length) {
+                await interaction.followUp({ content: "No results were found!" });
+                return;
+            }
+
+            const queue = player.nodes.create(interaction.guild, {
+                metadata: interaction.channel
+            });
+
+            try {
+                if (!queue.connection) {
+                    await queue.connect(interaction.member.voice.channel);
+                }
+            } 
+            catch {
+                player.nodes.delete(interaction.guildId);
+                await interaction.followUp({ content: "Could not join your voice channel!" });
+                return;
+            }
+
+            const playlist = searchResult.tracks;
+            const playlistLength = playlist.length;
+            await interaction.followUp({ content: `⏱ | Loaded **${playlistLength} track(s)** for query **${query}**` });
+            queue.addTrack(playlist);
+            if (!queue.isPlaying()) {
+                await queue.node.play();
+            }
+            return;
         }
     },
 };
